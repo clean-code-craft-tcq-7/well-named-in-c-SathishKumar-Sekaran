8fe080f8e8ab00521e5f6324aaf08f1b30a22949
SathishKumar-Sekaran <115685997+SathishKumar-Sekaran@users.noreply.github.com>
2022-11-22 21:43:39 +0530

    updated header file if condition

--- main.h
+++ main.h
@@ -5,3 +5,4 @@
 void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 void testPairToNumber( enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
 	
+#endif
