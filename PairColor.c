0bbc1aed5a5b45984fac63bd53f1db550540e039
SathishKumar-Sekaran <115685997+SathishKumar-Sekaran@users.noreply.github.com>
2022-11-22 22:14:16 +0530

    Deleted Const variable

--- PairColor.c
+++ PairColor.c
@@ -9,8 +9,6 @@ const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
 
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-
 int numberOfMinorColors =
     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
