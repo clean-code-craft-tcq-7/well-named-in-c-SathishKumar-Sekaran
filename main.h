c6c9d9affba4d89d20d70fe1860c5a6a42962733
SathishKumar-Sekaran <115685997+SathishKumar-Sekaran@users.noreply.github.com>
2022-11-22 21:11:06 +0530

    Add a sub function header files

---
+++ main.h
@@ -0,0 +1,7 @@
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+void testPairToNumber( enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
+	
